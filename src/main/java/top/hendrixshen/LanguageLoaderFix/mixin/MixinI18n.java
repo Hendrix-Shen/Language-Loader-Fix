3ec6499f06fc89d606b600d33d8336addbb22ca3
plulsls <plusls@qq.com>
2021-07-29 01:27:34 +0800

    Fix crash, support TranslatableText

--- src/main/java/top/hendrixshen/LanguageLoaderFix/mixin/MixinI18n.java
+++ src/main/java/top/hendrixshen/LanguageLoaderFix/mixin/MixinI18n.java
@@ -5,6 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.IllegalFormatException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,15 +14,26 @@ public class MixinI18n {
     // from Language.class
     private static final Pattern TOKEN_PATTERN = Pattern.compile("%(\\d+\\$)?[\\d.]*[df]");
 
-    @Redirect(method = "translate", at = @At(value = "INVOKE_ASSIGN", target = "Ljava/lang/String;format(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;"))
+    @Redirect(method = "translate", at = @At(value = "INVOKE", target = "Ljava/lang/String;format(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;"))
     private static String myFormat(String format, Object[] args) {
-        String newFormat = TOKEN_PATTERN.matcher(format).replaceAll("%s");
+        String newFormat = TOKEN_PATTERN.matcher(format).replaceAll("%$1s");
         Matcher matcher = TOKEN_PATTERN.matcher(format);
         int matcher_start = 0;
         for (int i = 0; i < args.length && matcher.find(matcher_start); ++i) {
-            args[i] = String.format(TOKEN_PATTERN.matcher(format).group(), args[0]);
+            String fmt = TOKEN_PATTERN.matcher(format).group();
+            try {
+                String formatResult = String.format(fmt, args[i]);
+                if (!formatResult.equals(fmt)) {
+                    args[i] = formatResult;
+                }
+            } catch (IllegalFormatException ignored) {
+
+            }
+
             matcher_start = matcher.end();
         }
         return String.format(newFormat, args);
     }
+
+
 }
